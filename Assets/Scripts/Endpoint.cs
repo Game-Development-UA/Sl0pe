0e453a5540e7428a0f6ae7ee850354b124664919
Andy <anguyen5412@email.arizona.edu>
2019-04-04 10:14:40 -0700

    Local Push

---
+++ Assets/Scripts/Endpoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endpoint : MonoBehaviour {
+<<<<<<< HEAD:Assets/Endpoint.cs
+//
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+=======
+
+    private bool complete = false;
+
+	private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MainCamera")
+        {
+            complete = true;
+        }
+    }
+
+    public bool GetComplete()
+    {
+        return complete;
+    }
+>>>>>>> Andy's_Development:Assets/Scripts/Endpoint.cs
+}
