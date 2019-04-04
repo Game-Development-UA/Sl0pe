31337a63dabce5d7c162403d0fd6d3829d344f33
Thomas Frauenfeld <tfrauenfeld@email.arizona.edu>
2019-04-04 09:10:26 -0700

    Upgraded Unity Version and set up player rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     private Vector2[] corners = new Vector2[4];
-    private float[] rotations = new float[4];
 
     public void InstantiatePlayer(Maze currentMaze)
     {
@@ -14,12 +13,6 @@ public class PlayerController : MonoBehaviour {
         corners[1] = new Vector2(-1 * currentMaze.size.x / 2 + 0.5f, currentMaze.size.z / 2 - 0.5f);
         corners[2] = new Vector2(-1 * currentMaze.size.x / 2 + 0.5f, -1 * currentMaze.size.z / 2 + 0.5f);
         corners[3] = new Vector2(currentMaze.size.x / 2 - 0.5f, -1 * currentMaze.size.z / 2 + 0.5f);
-
-        // Store rotational angles to match the 4 maze corners
-        rotations[0] = 180f;
-        rotations[1] = 180f;
-        rotations[2] = 0f;
-        rotations[3] = 0f;
 
         int cameraCorner = Random.Range(0, 4);
 
@@ -32,7 +25,36 @@ public class PlayerController : MonoBehaviour {
         else
         {
             transform.position = new Vector3(corners[cameraCorner].x, 0.5f, corners[cameraCorner].y);
-            transform.rotation = Quaternion.Euler(0f, rotations[cameraCorner], 0f);
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+            RaycastHit hit;
+
+            for (int i = 0; i < 4; ++i)
+            {
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+                {
+                    if ((hit.collider.tag == "Wall") && (hit.distance < 1f))
+                    {
+                        transform.Rotate(0f, 90f, 0f);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(0f, 90f, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(0f, -90f, 0f);
         }
     }
 }
