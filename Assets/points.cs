1f4330c0c93e4b95ed1b6e9061e438dbc95424a7
Andy Nguyen <anguyen5412@email.arizona.edu>
2019-04-25 12:41:16 -0700

    Fixing point system

---
+++ Assets/points.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombiesKilled : MonoBehaviour
+{
+    public int points = 0;
+    public Text pointsText;
+
+    public int zombiesKilled = 0;
+    public Text killsText;
+
+    public void IncrementPoints()
+    {
+        points += 10;
+        zombiesKilled += 1;
+    }
+
+    void Update()
+    {
+        killsText.text = zombiesKilled.ToString("KILLS : 00");
+        pointsText.text = points.ToString("POINTS : 00");
+    }
+}
