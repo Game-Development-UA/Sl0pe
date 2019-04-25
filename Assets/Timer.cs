a1f84264f308a7149f3b6befd0086f105785c6b5
Andy Nguyen <anguyen5412@email.arizona.edu>
2019-04-25 14:00:46 -0700

    Count UP to 30 = Loss on endless

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -8,24 +8,20 @@ using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    public float time = 50;
+    public float time = 0;
     //public static float saved_time = 0;
     public Text timeText;
 
 
     void Update()
     {
-        timeText.text = "TIME : " + (time - Time.deltaTime);
-
-        if (time <= 0)
-        {
-            SceneManager.LoadScene(6);
-
-        }
-        /*
         timeText.text = time.ToString("TIME : 00");
         time += Time.deltaTime;
-        */
+
+        if (time == 30)
+        {
+            SceneManager.LoadScene(6);
+        }
     }
 
     public float GetTime()
