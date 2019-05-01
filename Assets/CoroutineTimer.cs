95cf2e424ed37fd1a814cd50eab4c6c1a1376375
Thomas Frauenfeld <tfrauenfeld@email.arizona.edu>
2019-04-30 22:00:53 -0700

    Coroutine Stuff

---
+++ Assets/CoroutineTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoroutineTimer : MonoBehaviour
+{
+    public Text countdownTimer;
+
+    void Start()
+    {
+        StartCoroutine(gameCountdown());
+    }
+
+    private IEnumerator gameCountdown()
+    {
+        float seconds = 3f;
+        float normalizedTime = 0;
+
+        while (normalizedTime <= 1f)
+        {
+            countdownTimer.text = "" + normalizedTime;
+            normalizedTime += Time.deltaTime / seconds;
+            yield return null;
+        }
+    }
+}
